2848f6e384bf452decefef00386747dad3bcb988
FenixStupp2750 <FenixStupp2750@mail.ru>
2024-08-19 12:11:49 +0300

    first

---
+++ Deck.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef DECK_H
+#define DECK_H
+#include "Queue.h"
+class Deck : public Queue {
+public:
+	void Push(int value, int pushFront) final;
+	int Pop(int index) final;
+};
+
+#endif // !DECK_H
